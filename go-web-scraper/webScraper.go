25bb38be5d60aaab80841a7eb98be03a6271bec2
Jack <101360909+mahj79@users.noreply.github.com>
2023-12-29 15:29:22 -0600

    "added struct for nfl_odds for what information we want to pull. Also added error handling for file creation."

--- go-web-scraper/webScraper.go
+++ go-web-scraper/webScraper.go
@@ -4,16 +4,43 @@ package main
 
 import (
 	"fmt"
-	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/v2"
 	"encoding/csv"
 	"log"
 	"os"
+	"strings"
 )
 
+type nfl_odds struct {
+	game_date string
+	home_team string
+	away_team string
+	game_time string
+	game_odds string
+}
+
 func main() {
-	collector := colly.NewCollector()
+	fName := "nfl_odds.csv"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Cannot create file %q: %s\n", fName, err)
+		return
+	}
+	defer file.Close()
 
-	collector.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+	c := colly.NewCollector(
+		//Set allowable domains to only espn.com and www.espn.com
+		colly.AllowedDomains("espn.com", "www.espn.com"),
+		//Cache responses to only perform one download of pages
+		colly.CacheDir("./espn_cache"),
+	)
 
-	collector.Visit("https://espn.com/")
+	// Create another collector that is used to scrape details
+	detailCollector := c.Clone()
+
+	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+
+	c.Visit("https://www.espn.com/nfl/scoreboard")
+
+
 }
