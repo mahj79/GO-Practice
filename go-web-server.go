dad78bc442ef91b692594dd5586a8030214905f0
mahj79 <mahonej79@gmail.com>
2024-01-10 18:59:59 -0600

    basic function to get web server up and running on port 8000

--- go-web-server.go
+++ go-web-server.go
@@ -1,1 +1,22 @@
 // go practice building a web server
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Building a web server!")
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello World\n")
+		io.WriteString(w, r.Method)
+	}
+	http.HandleFunc("/", h1)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
