dff9e48302d4fb5c1aafc7b9f3236019b7017ac8
mahj79 <mahonej79@gmail.com>
2024-01-13 15:14:27 -0600

    finished project of building a go web server with htmx

--- go-web-server/go-web-server.go
+++ go-web-server/go-web-server.go
@@ -7,16 +7,18 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Film struct {
-	Title string
+	Title    string
 	Director string
 }
 
 func main() {
 	fmt.Println("Building a web server!")
 
+	//first handler function to display films described below
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
 		films := map[string][]Film{
@@ -28,15 +30,20 @@ func main() {
 		}
 		tmpl.Execute(w, films)
 	}
+
+	//second handler function for inputing data into the form and displaying entries
 	h2 := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep((1 * time.Second))
 		title := r.PostFormValue("title")
 		director := r.PostFormValue("director")
-		fmt.Print(title)
-		fmt.Print(director)
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
 	}
 
+	//handler functions for h1 and h2 functions built above
 	http.HandleFunc("/", h1)
 	http.HandleFunc("/add-film/", h2)
 
+	//handle requests for port 8000
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
