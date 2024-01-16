31976aab453d92d388ffb277a8bf030d31e0ee19
mahj79 <mahonej79@gmail.com>
2024-01-15 18:22:24 -0600

    updated header writing to output json and adding in new file for simulating storage

--- go-crud-api/api.go
+++ go-crud-api/api.go
@@ -11,8 +11,8 @@ import (
 )
 
 func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(v)
 }
 
@@ -32,11 +32,13 @@ func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
 
 type APIServer struct {
 	listenAddr string
+	store Storage
 }
 
-func NewAPIServer(listenAddr string) *APIServer {
+func NewAPIServer(listenAddr string, store Storage) *APIServer {
 	return &APIServer{
 		listenAddr: listenAddr,
+		store: store,
 	}
 }
 
---
+++ go-crud-api/storage.go
@@ -0,0 +1,20 @@
+//file for storage code
+
+package main
+
+import "database/sql"
+
+type Storage interface {
+	CreateAccount(*Account) error
+	DeleteAccount(int) error
+	UpdateAccount(*Account) error
+	GetAccountByID(int) (*Account, error)
+}
+
+type PostgresStore struct {
+	db *sql.DB
+}
+
+func NewPostgresStore() (*PostgresStore, error) {
+	
+}
