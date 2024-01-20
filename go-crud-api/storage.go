bfa8483bf7b09e106eaa76b58217695bb3631da9
mahj79 <mahonej79@gmail.com>
2024-01-20 15:35:55 -0600

    updated CreateAccount function in storage file to handle new account information into the db

--- go-crud-api/storage.go
+++ go-crud-api/storage.go
@@ -4,6 +4,8 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+
 	_ "github.com/lib/pq"
 )
 
@@ -52,7 +54,23 @@ func (s *PostgresStore) createAccountTable() error {
 	return err
 }
 
-func (s *PostgresStore) CreateAccount(*Account) error {
+func (s *PostgresStore) CreateAccount(acc *Account) error {
+	query := `insert into account
+	(first_name, last_name, number, balance, created_at)
+	values ($1, $2, $3, $3, $5)`
+	resp, err := s.db.Query(
+		query,
+		acc.FirstName,
+		acc.LastName,
+		acc.Number,
+		acc.Balance, acc.CreatedAt)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%+v\n", resp)
+
 	return nil
 }
 
