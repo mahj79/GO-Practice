402ab5c90c477f6d93dfe5c27df565ca3baaba7b
mahj79 <mahonej79@gmail.com>
2024-02-02 16:15:17 -0600

    fixed connection to db

--- go-crud-api/storage.go
+++ go-crud-api/storage.go
@@ -22,7 +22,7 @@ type PostgresStore struct {
 }
 
 func NewPostgresStore() (*PostgresStore, error) {
-	connStr := "user=postgres dbname=postgres password=gopractice sslmode=disable"
+	connStr := "user=postgres dbname=postgres password=gobankproject sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
