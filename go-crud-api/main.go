a71230fb34fb4e8e3657b7a9c00a546ac9d78437
mahj79 <mahonej79@gmail.com>
2024-02-11 14:02:35 -0600

    created new func in main file for seeAccount

--- go-crud-api/main.go
+++ go-crud-api/main.go
@@ -4,6 +4,19 @@ package main
 
 import "log"
 
+func seedAccount(store Storage, fname, lname, pw string) *Account {
+	acc, err := NewAccount(fname, lname, pw)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.CreateAccount(acc); err != nil {
+		log.Fatal(err)
+	}
+
+	return acc
+}
+
 func main() {
 	store, err := NewPostgresStore()
 	if err != nil {
